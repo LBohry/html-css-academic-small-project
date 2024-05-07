75a121a02d78f315f49b0aca3b6b2d3e5c43bfaa
Riadhbelgacem1 <riadh.belgacem@ensi-uma.tn>
2024-05-07 15:13:34 +0100

    0gir

---
+++ pages/fetch_users.php
@@ -0,0 +1,15 @@
+<?php
+header('Content-Type: application/json');
+require "functions.php";
+$errors = array();
+$sql = "SELECT username, email, password, date FROM users";
+$stmt = $pdo->query($sql);
+
+$users = [];
+while ($row = $stmt->fetch())
+{
+    $users[] = $row;
+}
+
+echo json_encode($users);
+?>
